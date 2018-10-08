8731fd9746b5248e3db86a7be96a8eb2c94e696b
root_global <root_global@atguigu.com>
2018-10-08 08:03:54 +0800

    test fallback

--- src/main/java/com/atguigu/entity/AddTest.java
+++ src/main/java/com/atguigu/entity/AddTest.java
@@ -6,8 +6,11 @@ public class AddTest {
 		System.out.println("testGit03 first commit,hello");
 		System.out.println("testGit04 helloworld");
 		System.out.println("testGit03 helloworld");
+<<<<<<< master
 		System.out.println("testGit03 group branch");
 		System.out.println("testGit04 department branch");
+=======
+>>>>>>> 6ef78b5 testGit03 Group branch
 	}
 
 }
